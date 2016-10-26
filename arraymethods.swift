13360f7a72d00df5d130b7bcb48653ee6fc735ee
Stephanholly <sholly@usc.edu>
2016-10-26 12:17:49 -0700

    first commit

---
+++ arraymethods.swift
@@ -0,0 +1,45 @@
+
+// use an array method that will sort the array alphabetically
+var words = ["watermelon", "strawberry", "apple", "banana", "peach"]
+
+
+
+
+
+// use an array method that will sort the array numerically from smallest to largest
+var nums = [5, 15, 104, 23, 19]
+
+
+
+
+
+// use an array method to check to see if "chelsea" is included in the teams array
+var teams = ["real madrid", "chelsea", "manchester united", "arsenal", "liverpool"]
+
+
+
+
+
+// use an array method that returns the array reversed
+var cities = ["phoenix", "san diego", "seattle", "los angeles", "austin"]
+
+
+
+
+// use an array method that returns the first item in the array
+var cities = ["phoenix", "san diego", "seattle", "los angeles", "austin"]
+
+
+
+
+
+// use an array method that returns the last item in the array
+var cities = ["phoenix", "san diego", "seattle", "los angeles", "austin"]
+
+
+
+
+// use an array method that returns the number of items in the array
+var cities = ["phoenix", "san diego", "seattle", "los angeles", "austin"]
+
+
